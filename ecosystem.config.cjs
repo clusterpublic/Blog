8ba55b0ca84f637159fe26493348fb53d399d634
Hamza Rizvi <hamza@brew.tv>
2026-06-15 23:14:38 +0530

    fixed ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -14,11 +14,9 @@ module.exports = {
       env_file: '.env',
       env: {
         NODE_ENV: 'development',
-        PORT: 5001,
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 5001,
       },
       error_file: 'logs/pm2-error.log',
       out_file: 'logs/pm2-out.log',
